dfd9d6887050f70d32e9c68b43b1122a413012db
Sanujit Majhi <sanujitmajhi473@gmail.com>
2025-11-13 11:32:54 +0000

    feat: add Radio and RadioGroup components

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import { Button } from "@/components/button";
 import { Checkbox } from "@/components/ui/Checkbox";
+import { Radio, RadioGroup } from "@/components/ui/Radio";
 
 import Image from "next/image";
 import { useState } from "react";
@@ -12,7 +13,10 @@ import { useState } from "react";
 export default function Home() {
 
 
-  const [agree,setAgree]=useState(false)
+  const [agree, setAgree] = useState(false)
+
+  const [gender, setGender] = useState("male");
+
 
 
 
@@ -20,40 +24,59 @@ export default function Home() {
 
   return (
     <>
-    <div className="flex flex-col justify-center items-center pt-4">
-      <Button disabled={false} text="Button" size="small" type="solid"/>
-    
-    </div>
-    
 
+      {/* /////////////////////
 
-    <div className="p-10">
+      Button test 
 
-      <Checkbox
+    //////////////////////
+     */}
+      <div className="flex flex-col justify-center items-center pt-4">
+        <Button disabled={false} text="Button" size="small" type="solid" />
 
-      label="Accept Terms & Conditions"
-      checked={agree}
-      onChange={(e) => setAgree(e.target.checked)}
-      
-      />
-        <p className="mt-4">Checked: {agree.toString()}</p>
+      </div>
 
+      {/* /////////////////////
 
-    </div>
+      Checkbox test 
 
+    ////////////////////// */}
 
-    <Checkbox error label="Error State" />
+      <div>
+        <Checkbox label="Accept Terms & Conditions" />
 
-    <Checkbox label="Focus Test" />
-    <Checkbox label="Hover Test" />
+      </div>
 
 
 
+      {/* /////////////////////
+
+      Radion box  
+
+    ////////////////////// */}
+
+      <div>
+
+        <RadioGroup
+          name="gender"
+          value={gender}
+          onChange={setGender}
+          error={false}
+        >
+          <Radio label="Male" value="male" />
+          <Radio label="Female" value="female" />
+          <Radio label="Other" value="other" />
+        </RadioGroup>
+
+      </div>
 
 
 
 
-    
+
+
+
+
     </>
   );
 }
--- components/ui/Checkbox.tsx
+++ components/ui/Checkbox.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 
 
 // We are adding the label and error because we are going to use InputelementAttribute which has everything 
@@ -30,23 +30,33 @@ export const Checkbox = ({
 
     return (
         <label
-            className={`flex items-center gap-3 select-none
-        ${isDisabled ? "cursor-not-allowed opacity-60" : "cursor-pointer"}
-      `}
-            onClick={handleFocusForward}   
+            className={`flex items-center gap-3 select-none ${isDisabled ? "cursor-not-allowed opacity-60" : "cursor-pointer"
+                }`}
+            onClick={handleFocusForward}
         >
-          
+
+
+            {/* //////////////////////
+           interactive input
+           ////////////////////// */
+            }
+
             <input
                 ref={inputRef}
                 type="checkbox"
                 {...rest}
-                className="peer sr-only"
+                className="peer absolute opacity-0 h-6 w-6"
             />
 
-            
+            {/* //////////////////////
+            This visual  checkbox 
+            ////////////////////// */}
+
             <span
                 className={`
           w-6 h-6 rounded-md flex items-center justify-center border-2 transition-all
+          [&>svg]:opacity-0 
+          peer-checked:[&>svg]:opacity-100
 
           ${error ? "border-[#FC5457]" : "border-gray-300"}
           peer-hover:border-[#703AE6]
@@ -54,28 +64,31 @@ export const Checkbox = ({
           peer-focus:border-[#F845FC]
           peer-focus:ring-4 peer-focus:ring-[#F845FC]/30
 
-          /* CHECKED */
           peer-checked:border-[#703AE6]
           peer-checked:bg-[#703AE6]
+          peer-checked:ring-4 peer-checked:ring-[#703AE6]/40
 
           ${className}
         `}
             >
                 <svg
-                    className="w-4 h-4 text-white opacity-0 peer-checked:opacity-100 transition"
-                    stroke="currentColor"
-                    strokeWidth="3"
+                    xmlns="http://www.w3.org/2000/svg"
                     fill="none"
                     viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-5 h-4 text-white transition"
                 >
-                    <path d="M5 13l4 4L19 7" />
+                    <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 6 6 9-13.5" />
                 </svg>
             </span>
 
+
+
+
+
             {label && (
-                <span className={`${error ? "text-[#FC5457]" : ""}`}>
-                    {label}
-                </span>
+                <span className={`${error ? "text-[#FC5457]" : ""}`}>{label}</span>
             )}
         </label>
     );
