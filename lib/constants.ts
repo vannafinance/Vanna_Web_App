a3d11ca6d3dd65c2bec308ed875a4493d78a9de4
divyansh <divyanshgoel8860@gmail.com>
2025-11-14 12:02:49 +0530

    Lib: Added Constants File

---
+++ lib/constants.ts
@@ -0,0 +1,36 @@
+export const dropdownItems = [
+    {
+        title:"Nothing",
+        link:"/href"
+    },
+    {
+        title:"Link 2",
+        link:"/href"
+    },
+    {
+        title:"Link 3",
+        link:"/href"
+    }
+]
+
+export const navbarItems = [
+    {
+        title:"Portfolio",
+        link:"/portfolio"
+    },
+    {
+        title:"Earn",
+        link:"/earn"
+    },
+    {
+        title:"Margin",
+        link:"/margin"
+    },
+    {
+        title:"Trade",
+        link:"/trade"
+    },{
+        title:"Analytics",
+        link:"/analytics"
+    }
+]
