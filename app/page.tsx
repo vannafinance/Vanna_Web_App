8ec3511326c2b22a0e2aad0312a7a5642c4a3de2
Sanujit Majhi <sanujitmajhi473@gmail.com>
2025-11-17 06:30:27 +0000

    ComponentUpdate

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,46 @@
+"use client"
+
+
 import { Button } from "@/components/button";
+import { Checkbox } from "@/components/ui/Checkbox";
+import { Radio, RadioGroup } from "@/components/ui/RadioButton";
+import ToggleButton from "@/components/ui/Toogle";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
+
+  const [ischecked,setIscheckd]=useState(false)
+
+
   return (
+    <>
+    
     <div className="flex flex-col justify-center items-center pt-4">
       <Button disabled={false} text="Button" size="small" type="solid"/>
     </div>
+
+    <div>
+    <Checkbox />
+
+    </div>
+    
+    <div>
+
+      <RadioGroup name="sanu">
+        <Radio/>
+      </RadioGroup>
+    
+    </div>
+
+    <div>
+    <ToggleButton onToggle={(value) => console.log("State:", value)} />
+
+    </div>
+
+
+
+    </>
+
   );
 }
